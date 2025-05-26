f2099dfdc99c4c259bb4313126f881d9d7b933d4
Socheat Sok <socheatsok78@gmail.com>
2025-05-26 19:52:27 +0700

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
               mkdir -p $out/{dist,bin}
               ${pkgs.yarn}/bin/yarn build --outDir $out/dist
               echo -e "#!/usr/bin/env bash\ncaddy file-server --browse \"\$@\" $out/dist" > $out/bin/vue-project
-
+              chmod +x $out/bin/vue-project
             '';
 
             meta = { };
